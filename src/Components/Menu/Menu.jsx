c42c96241a888de4795776f64cabd6f1fca185a9
mikeyBlah <redburnmichael@gmail.com>
2020-09-24 08:22:31 +1000

    Menu hide on menu item click

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -11,6 +11,11 @@ const Menu = (props) => {
     changeIsActive(isActiveNew);
   } 
 
+  const handelMenuItemClick = (indx) => {
+    click(indx);
+    handleMenuClick();
+  }
+
   return (
     <div className={`${styles.menuContainer} ${isActive ? styles.active : ''}`}>
       <div className={`${styles.hamburgerContainer} ${isActive ? styles.active : ''}`} onClick={handleMenuClick}>
@@ -19,11 +24,11 @@ const Menu = (props) => {
         <span></span>
       </div>
       <ul>
-        <li onClick={() => click(0)}>Home</li>
-        <li onClick={() => click(1)}>Item One</li>
-        <li onClick={() => click(2)}>Item Two</li>
-        <li onClick={() => click(3)}>Item Three</li>
-        <li onClick={() => click(4)}>Item Four</li>
+        <li onClick={() => handelMenuItemClick(0)}>Home</li>
+        <li onClick={() => handelMenuItemClick(1)}>Item One</li>
+        <li onClick={() => handelMenuItemClick(2)}>Item Two</li>
+        <li onClick={() => handelMenuItemClick(3)}>Item Three</li>
+        <li onClick={() => handelMenuItemClick(4)}>Item Four</li>
       </ul>
     </div>
   )
